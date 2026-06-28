5b5105e26c868a9acdcd6fab5f3048823f711785
tosuki <okaabe2006@gmail.com>
2026-06-28 16:42:45 -0300

    delete: Remove mcp-api

--- mcp-api/src/handlers/handler.ts
+++
@@ -1,5 +0,0 @@
-import { Job } from "../model/job";
-
-export interface Handler {
-    handle(job: Job, updateProgress: (progress: number) => unknown): Promise<Job> | Job
-}
--- mcp-api/src/model/vacancy.ts
+++
@@ -1,7 +0,0 @@
-export type Vacancy = {
-    name: string
-    description: string
-    requirements: string[]
-    createdAt: number
-    createdBy: string
-}
